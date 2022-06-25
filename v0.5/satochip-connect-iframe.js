c6fc2882147a03e8a06fe1090ff94cdc301ddfa9
Toporin <satochip.wallet@gmail.com>
2022-06-25 21:02:21 +0100

    Satochip-Connect v0.5.0: use version folder for backward compatibility
    Changes with interoperability impact are put in a new folder to ensure backward compatibility.
    Legacy version remains located in the root folder.

--- v0.5/satochip-connect-iframe.js
+++ v0.5/satochip-connect-iframe.js
@@ -6,8 +6,8 @@ if (window.parent !== window) {// iframe
   //==================
   obj_iframe.setupListenersIframe= function() {
     console.log('In satochip-connect-iframe: setupListenersIframe START')
-    const tabDomain = 'https://toporin.github.io/Satochip-Connect/'
-    //const tabDomain = 'http://localhost:3000'
+    //const tabDomain = 'https://toporin.github.io/Satochip-Connect/v0.5'
+    const tabDomain = 'http://localhost:3000/v0.5'
 
     // Open as IFRAME
     onmessage = async ({ data, source, origin }) => {
@@ -19,28 +19,29 @@ if (window.parent !== window) {// iframe
         const tab = obj_iframe.openOnce(tabDomain, 'satochip-tab');
 
         tab.onbeforeunload = () => {
+          console.log('In satochip-connect-iframe: setupListenersIframe: onmessage: CLOSING TAB');
           obj_iframe.tabReady = false;
         };
-        
+
         console.log('In satochip-connect-iframe: setupListenersIframe: onmessage: NEWTAB MIDDLE1')
         if (!obj_iframe.tabReady) obj_iframe.pingTab();
         while (!obj_iframe.tabReady) {
           console.log(`blocking`);
           await obj_iframe.sleep(1000);
-          
+
           if (!obj_iframe.tabReady) obj_iframe.pingTab();
-          
+
           //console.log('In satochip-connect-iframe: setupListenersIframe: onmessage: in while: setting ready flag TRUE')
           //obj_iframe.tabReady = true;
           //obj_iframe.bc.postMessage({ target: 'SATOCHIP-TAB', action: 'satochip-connection-check' });
           //console.log('In satochip-connect-iframe: setupListenersIframe: onmessage: in while: setting ready flag done!')
-          
+
         }
-        
+
         console.log('In satochip-connect-iframe: setupListenersIframe: onmessage: NEWTAB MIDDLE2')
         data.target = 'SATOCHIP-TAB';
         obj_iframe.bc.postMessage(data, '*');
-        
+
         console.log('In satochip-connect-iframe: setupListenersIframe: onmessage: NEWTAB END')
       }
       console.log('In satochip-connect-iframe: setupListenersIframe: onmessage END')
@@ -56,7 +57,7 @@ if (window.parent !== window) {// iframe
         obj_iframe.sendMessageToExtension(data);
       }
     };
-    
+
     console.log('In satochip-connect-iframe: setupListenersIframe END')
   }
 
@@ -90,7 +91,7 @@ if (window.parent !== window) {// iframe
     return new Promise((resolve) => setTimeout(resolve, ms));
     console.log('In satochip-connect-iframe: sleep END')
   }
-  
+
   obj_iframe.tabReady = false;
   obj_iframe.bc = new BroadcastChannel('satochip');
   obj_iframe.setupListenersIframe();
